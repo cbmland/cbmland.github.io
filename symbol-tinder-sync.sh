1b575a09cac20a8744883252eed458d139c4cd9d
cbmland <cbm@live.com>
2021-12-20 23:00:33 +0800

    push static tinder

---
+++ symbol-tinder-sync.sh
@@ -0,0 +1,11 @@
+###
+ # @Author: your name
+ # @Date: 2021-12-20 22:43:12
+ # @LastEditTime: 2021-12-20 22:56:53
+ # @LastEditors: Please set LastEditors
+ # @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ # @FilePath: /cbmland.github.io/cats/symbol-tinder/sync.sh
+### 
+
+rm -r ./symbol-tinder/*
+cp -r ~/Dropbox/Projects/node/Symbol-Tinder/dist/ ./symbol-tinder/
